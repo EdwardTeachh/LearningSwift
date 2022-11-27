82454daa704bfbb305479f74b236fc64fb77c4aa
EdwardTeachh <85226936+EdwardTeachh@users.noreply.github.com>
2022-11-27 18:09:43 +0600

    update

--- GeekBrains/SwiftBasics/SwiftBasics/main.swift
+++ GeekBrains/SwiftBasics/SwiftBasics/main.swift
@@ -1,29 +1,129 @@
-
 import Foundation
 
-// MARK: - 1. Написать функцию, которая определяет, четное число или нет.
+// 1. Описать несколько структур – любой легковой автомобиль SportCar и любой грузовик TrunkCar.
+//2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+//3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+//4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
+//5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
+//6. Вывести значения свойств экземпляров в консоль.
 
-func evenNumber(_ value: Int) -> Bool {
-    return value % 2 == 0
+// MARK: - Перечисления
+
+enum windowCar: String {
+    case open = "окна открыты"
+    case closed = "окна закрыты"
 }
 
-// MARK: - 2. Написать функцию, которая определяет, делится ли число без остатка на 3.
-
-//var b = 55
-
-func divisionNumber(_ value: Int) -> Bool {
-    return value % 3 == 0 ? true : false
+enum engineState: String {
+    case Start = "Двигатель запущен"
+    case Stop = "Двигатель Остановлен"
 }
 
-//MARK: - 3. Создать возрастающий массив из 100 чисел.
-
-var xArray : Array<Int> =  [Int]()
-var i = 0
-while i<100 {
-    xArray.append(i+1)
-    i+=1
+enum baggageFill: String {
+    case Full = "Багажник заполнен полностью"
+    case Empty = "Багажник пуст"
+    case partially = "Багажник запонен на - "
+    case overload = "Перегруз багажа! попробуйте добавить меньше груза"
 }
 
-// MARK: - 4. Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+enum actionCar: String {
+    case openWindow = "Окткрыть окна"
+    case closedWindow = "Закрыть окна"
+    case startEngine = "Завести двигатель"
+    case stopEngine = "Заглушить двигатель"
+    case putLuggage = "Положить груз в багажник"
+    case removeLuggage = "Вытащить груз из багажника"
+    
+    
+}
 
-xArray.removeAll(where: {evenNumber($0) || !divisionNumber($0)})
+// MARK: - Структура
+
+struct car {
+    let brand:String // марка авто
+    let yearCar: Int // год выпуска
+    let baggageVolume:Int // объём багажника
+    var engine: engineState {
+        didSet{
+            print(engine.rawValue)
+        }
+    }
+    
+    var windows: windowCar {
+        didSet {
+            print(windows.rawValue)
+        }
+    }
+    
+    var volumebaggageLoading: Int   // колличество груза багажа
+    
+    mutating func loadingBaggage(_ volumebaggageLoading: Int){
+        if baggageVolume == volumebaggageLoading {
+            print(baggageFill.Full.rawValue)
+            
+        }
+        if baggageVolume > volumebaggageLoading {
+            print(baggageFill.partially.rawValue, baggageVolume - volumebaggageLoading," литров")
+        }
+        if baggageVolume < volumebaggageLoading {
+            print(baggageFill.overload.rawValue)
+            
+        }
+        else{
+            print(baggageFill.Empty.rawValue)
+        }
+    }
+    
+    func ActionCar(type: actionCar){
+        switch type {
+        case .openWindow:
+            print(windowCar.open.rawValue)
+        case .closedWindow:
+            print(windowCar.closed.rawValue)
+            
+        case .startEngine:
+            print(engineState.Start.rawValue)
+            
+        case .stopEngine:
+            print(engineState.Stop.rawValue)
+            //        case .putLuggage:
+            //            print(loadingBaggage.)
+            
+        default:
+            print("Zdelaite chtoto s auto")
+        }
+    }
+    
+    mutating func CarInfo(){
+        print("_______________________")
+        print("Модель авто - \(brand)")
+        print("Год выпуска - \(yearCar)")
+        print("Объём багажгика - \(baggageVolume)")
+    }
+    
+    
+    init(brand: String, yearCar: Int, baggageVolume: Int) {
+        self.brand = brand
+        self.yearCar = yearCar
+        self.baggageVolume = baggageVolume
+        self.engine = .Stop
+        self.windows = .closed
+        self.volumebaggageLoading = 0
+        
+        
+    }
+}
+var sprotCar = car(brand: "BMW", yearCar: 2020, baggageVolume: 1500)
+var truckCar = car(brand: "MAN", yearCar: 1999, baggageVolume: 50000)
+
+sprotCar.CarInfo()
+sprotCar.ActionCar(type: .openWindow)
+sprotCar.ActionCar(type: .startEngine)
+sprotCar.loadingBaggage(30000)
+
+
+
+truckCar.CarInfo()
+truckCar.ActionCar(type: .closedWindow)
+truckCar.ActionCar(type: .stopEngine)
+truckCar.loadingBaggage(20000)
