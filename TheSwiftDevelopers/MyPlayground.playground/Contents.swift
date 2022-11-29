190ad71b9ca07227652a11bafaa0e77f8917cb00
EdwardTeachh <85226936+EdwardTeachh@users.noreply.github.com>
2022-11-29 22:41:53 +0600

    L2

--- TheSwiftDevelopers/MyPlayground.playground/Contents.swift
+++ TheSwiftDevelopers/MyPlayground.playground/Contents.swift
@@ -1,11 +1,10 @@
 import UIKit
 
-var car = "bmw"
-let name = "Valera"
-let age = 25
-
-car
-name
-
-print(name + " na " + car)
-
+let age: Int = -50
+let key: UInt = 45
+let key2: Double = 443.443
+let key3: Float = 365.3344
+var bool: Bool = true
+var key4: Character = "!"
+let name: String = "Edward"
+typealias myType = Int
